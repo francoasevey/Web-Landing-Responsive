4d78682bb02fecf5546d39c09d7b287bea4dbd06
francoromeo <francoromeo5012@gmail.com>
2023-10-08 20:22:54 -0300

    js

---
+++ App.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.nav_menu');
+const menuList = document.querySelector('.nav_list');
+const links = document.querySelectorAll('.nav_link');
+
+menu.addEventListener('click', function() {
+    menuList.classList.toggle('nav_list--show');
+});
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        menuList.classList.remove('nav_list--show');
+    })
+})
